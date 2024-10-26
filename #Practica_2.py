042a8b2ef8008d453964c4c4f788671de7500eea
LauCat23 <lauragc@jarandes.com>
2024-10-25 22:35:52 -0500

    Variables

---
+++ #Practica_2.py
@@ -0,0 +1,23 @@
+
+
+# La actividad a realizar es:
+# Actividad 4: Ejercicio 1: Crear Variables
+# En este ejercicio, se te pide crear variables para almacenar datos personales.
+# Deberás definir:
+# 1. Tu edad como un número entero (int).
+mi_edad=22
+# 2. Tu altura en metros como un número de punto flotante (float).
+mi_altura=1.63
+# 3. Tu nombre completo como una cadena de texto (str).
+mi_nombre="Laura Galvis"
+# 4. Un valor booleano que indique si tienes mascota (bool).
+Mascota=True
+# 5. Una lista que contenga los nombres de tres amigos (list).
+Friends=["Miller","George","Gustavo"]
+
+
+print("Mi edad es:",mi_edad)
+print("Mido:",mi_altura)
+print("y me llamo:",mi_nombre)
+print("tengo mascota?",Mascota)
+print("y mis amigos son:",Friends)
